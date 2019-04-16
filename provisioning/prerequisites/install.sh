6670e9071397ba25ece0ab1af54b3b7049bc7989
devopsravikumar <44359515+devopsravikumar@users.noreply.github.com>
2019-04-16 15:51:39 +0530

    ip's changed

--- provisioning/prerequisites/install.sh
+++ provisioning/prerequisites/install.sh
@@ -7,10 +7,10 @@ figlet MASTER
 
 echo "[TASK 1] Add hosts to etc/hosts"
 cat >>/etc/hosts<<EOF
-100.10.10.100 k8s-master
-100.10.10.101 k8s-worker-1
-100.10.10.102 k8s-worker-2
-100.10.10.103 k8s-worker-3
+10.1.1.132 k8s-master
+10.1.1.133 k8s-worker-1
+10.1.1.134 k8s-worker-2
+10.1.1.135 k8s-worker-3
 EOF
 
 echo "[TASK 2] Disable SELINUX"
